bd0ab26c9a58e8ae565f6406f5f458420f0e2214
DownerCase <downercase8@gmail.com>
2023-08-21 12:02:00 +0100

    Update to 5.12 and tweak port

--- overlay_ports/ecal/portfile.cmake
+++ overlay_ports/ecal/portfile.cmake
@@ -5,8 +5,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO eclipse-ecal/ecal
-    REF v5.11.5
-    SHA512 5048f332e72892deeaf2d90b8e4193ef3e05187fbccde22ae1561fe40d2a9a2617749cf91a546f7dad456ecd2438e6d6d6b59304656b2e2769e7036100c37a99
+    REF v${VERSION}
+    SHA512 e9bc6c579a5331bdecc0384fb1cbac6bf63cb3910e26a4004d48c3a25528fed66413a2c80f4d866d3958d3800579c57a7298e61fca20adf9b24f5daa2e07ed3d
     HEAD_REF master
     PATCHES
         0001-disable-app-plugins.patch
@@ -33,7 +33,6 @@ vcpkg_cmake_configure(
         -DBUILD_PY_BINDING=OFF
         -DBUILD_CSHARP_BINDING=OFF
         -DBUILD_ECAL_TESTS=OFF
-        -DECAL_LAYER_ICEORYX=OFF
         -DECAL_INCLUDE_PY_SAMPLES=OFF
         -DECAL_INSTALL_SAMPLE_SOURCES=OFF
         -DECAL_JOIN_MULTICAST_TWICE=OFF
@@ -54,19 +53,20 @@ vcpkg_cmake_configure(
         -DECAL_THIRDPARTY_BUILD_HDF5=OFF
         -DCPACK_PACK_WITH_INNOSETUP=OFF
         -DDISABLE_FIND_PACKAGE_OVERLOAD=ON # From patch, disable find_package macro
-        -DECAL_BUILD_VERSION="5.11.5"
+        -DECAL_BUILD_VERSION="${VERSION}"
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME eCAL           CONFIG_PATH share/eCAL)
+vcpkg_cmake_config_fixup(PACKAGE_NAME CMakeFunctions CONFIG_PATH share/CMakeFunctions)
 
 # Remove extra debug files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# Don't need global ini files
+# global ini files not strictly required
 if (VCPKG_TARGET_IS_WINDOWS)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cfg" "${CURRENT_PACKAGES_DIR}/debug/cfg")
 else()
